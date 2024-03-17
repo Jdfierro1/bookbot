474b807a5efbd253158f4c72d5b5d5170a28b052
Jdfierro1 <Jdfierro1@gmail.com>
2024-03-16 20:54:27 -0500

    Final bookBot submission for final grade.

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
-def main():
+def read_text():
+    # This function reads the text of a called file and returns it as a string 
     with open("/Users/joshuafierro/workspace/github.com/Jdfierro1/bookbot/books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
-    # The above shows all text of the .txt file.
-    # below begins the process to count the words of the file
-    frankenstein = "/Users/joshuafierro/workspace/github.com/Jdfierro1/bookbot/books/frankenstein.txt"
-    text = get_book_text(frankenstein)
-    num_words = get_num_words(text)
-    print(f"{num_words} words found in the document!")
+        text = f.read()
+    return text 
 
-def get_num_words(text):
+def count_words(text):
+    # Count the number of words in the book and return it at the end
     words = text.split()
-    return len(words)
+    return f"{len(words)} words found in the document."
 
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
+def count_letters(text):
+    # This will count the unique count of letters in the book, all lower case 
+    letters = {}
+    text = text.lower()
+    for words in text:
+        if words.isalpha():
+            letters[words] = letters.get(words, 0) + 1
+    sorted_letters = list(letters.items())
+    sorted_letters.sort(key=lambda x: x[1], reverse=True)
+    return sorted_letters
 
-#### This was my first attempt below. Above is refined approach. ### 
-   # with open("/Users/joshuafierro/workspace/github.com/Jdfierro1/bookbot/books/frankenstein.txt") as f:
-   #     file_contents = f.read()
-   # print(file_contents)
-   # words = file_contents.split()
-   # print(f"This book has {len(words)} words!")
-
+def main():
+    text = read_text()
+    if text is not None:
+        print("--- Begin report of books/frankenstein.txt ---")
+        word_count = count_words(text)
+        print(word_count)
+        character_count = count_letters(text)
+        for char, count in character_count:
+            print(f"The '{char}' character was found {count} times")
+        print("--- End report ---")
 main()
